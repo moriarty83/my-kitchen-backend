586e74ed4d607f1cdf3df5eed22c6336b0ca1d3b
moriarty83 <moriarty83@gmail.com>
2022-01-07 16:09:12 -0500

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://my-kitchen-site.netlify.app/'
+    origins 'https://my-kitchen-site.netlify.app'
 
     resource '*',
       headers: :any,
