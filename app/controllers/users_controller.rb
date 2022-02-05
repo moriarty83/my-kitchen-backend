8fa7a450c36e86e12d28dd686dbd77a40643c15c
moriarty83 <moriarty83@gmail.com>
2022-02-05 13:38:20 -0500

    route change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -112,6 +112,22 @@ class UsersController < ApplicationController
           end
     end
 
+    def generate_password_token!
+        self.reset_password_token = generate_token
+        self.reset_password_sent_at = Time.now.utc
+        save!
+       end
+       
+       def password_token_valid?
+        (self.reset_password_sent_at + 4.hours) > Time.now.utc
+       end
+       
+       def reset_password!(password)
+        self.reset_password_token = nil
+        self.password = password
+        save!
+    end
+
   private
 
     def user_params 
@@ -138,6 +154,10 @@ class UsersController < ApplicationController
             end
         end
     end
+
+    def generate_token
+        SecureRandom.hex(10)
+       end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :recipes
   resources :user_ingredients
   resources :ingredients
+  resources :password
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resource :users, only: [:create]
@@ -12,6 +13,6 @@ Rails.application.routes.draw do
   put "/users", to: "users#update"
   post "/users/delete", to: "users#delete"
   get "/users/delete-request", to:"users#delete_request", as: 'user'
-  post '/password/forgot', to: 'passwords#forgot'
-  post '/password/reset', to: 'passwords#reset'
+  post '/password/forgot', to: 'password#forgot'
+  post '/password/reset', to: 'password#reset'
 end
